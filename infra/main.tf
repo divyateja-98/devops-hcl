d13e0cdec36b65337cfe5a19f0a80f4861d5e6ad
divyateja-98 <yadavadivyateja98@gmail.com>
2025-06-03 14:59:55 +0000

    updated file for terraform-2

--- infra/main.tf
+++ infra/main.tf
@@ -26,6 +26,8 @@ provider "aws" {
 }
 
 provider "kubernetes" {
+  # Add an explicit dependency on the EKS API readiness check
+  depends_on             = [null_resource.eks_api_ready]
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
