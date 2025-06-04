fb6fcbaa4cb5e5d203687eeceb7e39ad894aa29a
divyateja-98 <yadavadivyateja98@gmail.com>
2025-06-04 11:53:08 +0000

    New Devo Branch

--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-1-us-west-1"
     key            = "eks/terraform.tfstate"
     region         = "us-west-1"
     encrypt        = true
