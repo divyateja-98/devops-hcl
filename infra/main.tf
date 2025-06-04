dab7492408381658685b8cde32635c03a927adc5
divyateja-98 <yadavadivyateja98@gmail.com>
2025-06-04 11:05:16 +0000

    New Structure

--- infra/main.tf
+++ infra/main.tf
@@ -1,46 +1,42 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 3.17"
+  version = "5.21.0"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    "Name" = "eks-vpc"
-  }
+  name                 = "eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = ">= 17.1.0, < 18.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.27"
+  subnets         = module.vpc.private_subnets  # <-- CHANGE subnet_ids to subnets
 
-  subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-  manage_aws_auth = true
+  # Use managed node groups (recommended)
+  manage_aws_auth   = true  # optional depending on your setup
 
-  node_groups = {
-    default = {
+  managed_node_groups = {
+    node_group1 = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-      instance_types   = ["t3.medium"]
-      disk_size        = 20
+
+      instance_type = "t3.medium"
+      key_name      = "your-keypair" # if you use SSH
+      subnet_ids    = module.vpc.private_subnets
     }
   }
 
   tags = {
     Environment = "production"
-    Terraform   = "true"
   }
 }
