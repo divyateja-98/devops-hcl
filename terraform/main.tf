dc8812a23996ba2c0317ade47d4ffd820e1816dc
divyateja-98 <yadavadivyateja98@gmail.com>
2025-06-04 11:30:25 +0000

    New Devo Branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_cluster" "eks" {
 }
 
 resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
+  name = "eks-role-new"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
