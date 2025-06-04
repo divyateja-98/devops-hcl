512c6b9bd83ae9906cc867111e85c0a10f674579
divyateja-98 <yadavadivyateja98@gmail.com>
2025-06-04 11:01:20 +0000

    New Structure

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  version = ">= 3.17"
 
-  name = "${var.cluster_name}-vpc"
+  name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
@@ -13,155 +28,47 @@ module "vpc" {
   single_nat_gateway = true
 
   tags = {
-    Terraform   = "true"
-    Environment = "production"
+    "Name" = "eks-vpc"
   }
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
-  cluster_name    = var.cluster_name
-  cluster_version = var.kubernetes_version
+  version         = ">= 17.1.0, < 18.0.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   manage_aws_auth = true
-  enable_irsa     = true
 
   node_groups = {
-    eks_nodes = {
+    default = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-      instance_type    = "t3.medium"
+      instance_types   = ["t3.medium"]
+      disk_size        = 20
     }
   }
 
   tags = {
     Environment = "production"
+    Terraform   = "true"
   }
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+output "kubeconfig" {
+  value     = module.eks.kubeconfig
+  sensitive = true
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
-# Kubernetes NGINX Deployment + Service via Kubernetes provider
-
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name      = "nginx-deployment"
-    labels = {
-      app = "nginx"
-    }
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "nginx"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "nginx"
-        }
-      }
-
-      spec {
-        container {
-          name  = "nginx"
-          image = "nginx:latest"
-
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-service"
-  }
-
-  spec {
-    selector = {
-      app = "nginx"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-      node_port   = 30080
-    }
-
-    type = "NodePort"
-  }
-}
-
-# ArgoCD Setup via Helm
-
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
-  version    = "5.46.8"
-
-  set {
-    name  = "server.service.type"
-    value = "LoadBalancer"
-  }
-
-  wait    = true
-  timeout = 600
-}
-
-# ArgoCD Application CRD to deploy NGINX app from GitHub repo
-
-resource "kubernetes_manifest" "argocd_nginx_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name      = "nginx-app"
-      namespace = "argocd"
-    }
-    spec = {
-      project = "default"
-      source = {
-        repoURL        = "https://github.com/YOUR_GITHUB_USER/YOUR_NGINX_REPO.git"
-        targetRevision = "HEAD"
-        path           = "kubernetes-manifests"
-      }
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = "default"
-      }
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-      }
-    }
-  }
+output "cluster_ca_certificate" {
+  value = module.eks.cluster_certificate_authority_data
 }
